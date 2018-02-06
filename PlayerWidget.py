42c3f99ed5e54de6adef06b4f55553299caba600
Josh Grebler <jgreb314@gmail.com>
2018-02-05 19:54:29 -0500

    built portions of playerwidget UI

--- PlayerWidget.py
+++ PlayerWidget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget QVBoxLayout
 
 class PlayerWidget(QWidget):
 
@@ -11,13 +11,20 @@ class PlayerWidget(QWidget):
 	
 		for stat in stats:
 		
-# class StatWidget(QWidget):
+class StatWidget(QWidget):
 	
-	def __init__(self, statName, statValue):
-		self.statName = statName
-		self.statValue = statValue
+	def __init__(self, name, buff):
+		self.name = name
+		self.buff = buff
 		super().__init__()
 		self.initUI()
 		
 	def initUI(self):
+	
+		groupbox = QGroupBox(self.name,self)
+		statBox = QLineEdit(self)
+		
+		vbox = QVBoxLayout(self)
+		vbox.addWidget()
+		
 		
