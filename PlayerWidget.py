04273f7aaf0b135b0ed61191a59072a2db330ff0
Josh Grebler <jgreb314@gmail.com>
2018-02-12 19:07:41 -0500

    Moved parsing function to playerwidget file. Deleted parsing script file

--- PlayerWidget.py
+++ PlayerWidget.py
@@ -1,15 +1,23 @@
-from PyQt5.QtWidgets import QWidget QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
 class PlayerWidget(QWidget):
 
-	def __init__(self,stats):
+	def __init__(self, name, stats):
 		self.stats = stats
 		super().__init__()
 		self.initUI()
 		
 	def initUI(self):
+		for stat in self.stats:
+			#iterate through stats dict and add widgets
 	
-		for stat in stats:
+	def parseStats(file):
+	stats = {};
+	for line in file:
+		if not line.startswith('#'):
+			split = line.split('=')
+			stats[split[0]]=split[1]
+	return stats
 		
 class StatWidget(QWidget):
 	
