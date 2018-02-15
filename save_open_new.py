3f88ad7e1000694bc01c305f4572cacb99d4514d
Josh Grebler <jgreb314@gmail.com>
2018-02-15 12:18:07 -0500

    Added UI/Layout

--- save_open_new.py
+++ save_open_new.py
@@ -1,3 +1,5 @@
+from PyQt5.QtWidgets import QWidget, QFileDialog, QVBoxLayout, QHBoxLayout, QPushButton
+
 def export(data, file):
 	tdata = data
 	for key, value in tdata.items():
@@ -6,3 +8,29 @@ def export(data, file):
 				line = key + "=" + value
 			else:
 				file.append(key + "=" + value)
+				
+class SplashWidget(QWidget):
+	
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+	
+	def initUI(self):
+		
+		vbox = QVBoxLayout(self)
+		
+		logo = QLabel(self)
+		logo.setPixmap(QPixmap("logo-text.png"))
+		vbox.addWidget(logo)
+		
+		hbox = QHBoxLayout(self)
+		hbox.addStretch()
+		open_button = QPushButton(self, "Open Character")
+		new_button = QPushButton(self, "New Empty Character Template")
+		exit_button = QPushButton(self, "Exit Caves and Cheese")
+		hbox.addWidget(open_button)
+		hbox.addWidget(new_button)
+		hbox.addWidget(exit_button)
+		
+		vbox.addLayout(hbox)
+		self.setLayout(vbox)
