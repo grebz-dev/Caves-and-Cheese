de5fa3f3060b754fc86eff39e8fb33000db2abba
Josh Grebler <jgreb314@gmail.com>
2018-02-16 12:48:15 -0500

    Added handling for splash dialog

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
 from PlayerWidget import PlayerWidget
 from save_open_new import SplashWidget
-
-
+from PyQt5.QtCore import pyqtSlot
+	
+class MainWindow(QMainWindow):
+	
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+	
+	def initUI(self):
+		self.setWindowTitle('Caves & Cheese')
+		self.splash=SplashWidget()
+		self.setCentralWidget(self.splash)
+		self.move(300,500)
+		self.resize(0,self.height())
+		self.splash.fileOpened.connect(self.initGame)
+	
+	@pyqtSlot(str)
+	def initGame(self, filename):
+		player = PlayerWidget(filename)
+		self.setCentralWidget(player)
+		self.resize(0,self.height())
+		
 if __name__ == '__main__':
 
 	app = QApplication(sys.argv)
-
-	w = QWidget()
-	w.setWindowTitle('Caves & Cheese')
-	statfile = open("Paul.cnc")
-	player = PlayerWidget(statfile)
-	layout=QVBoxLayout()
-	layout.addWidget(SplashWidget())
-	layout.addWidget(player)
-	w.setLayout(layout)
-	w.move(300,500)
-	w.resize(0,w.height())
+	w = MainWindow()
 	w.show()
 	sys.exit(app.exec_())
