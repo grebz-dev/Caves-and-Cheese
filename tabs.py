0d8cf0d16d402258ef944c8b0534a7250319616a
Josh Grebler <jgreb314@gmail.com>
2018-03-24 21:10:52 -0400

    added skills tab

--- tabs.py
+++ tabs.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QWidget, QAction, QTabWidget,QVBoxLayout,QPushButton
 from Player import PlayerWidget
 from notes import NotesWidget
 from armor import ArmorWidget
+from skills import SkillWidget
 
 class MultiTabWidget(QWidget):
  
@@ -18,16 +19,19 @@ class MultiTabWidget(QWidget):
 		self.tab1 = QWidget()	
 		self.tab2 = QWidget()
 		self.tab3 = QWidget()
+		self.tab4 = QWidget()
 
 		# Add items to populate tabs
 		player = PlayerWidget(self.statFile)
 		armor = ArmorWidget()
 		notes = NotesWidget()
+		skills = SkillWidget()
  
 		# Add tabs
 		self.tabs.addTab(self.tab1,"Player")
 		self.tabs.addTab(self.tab2,"Armor")
 		self.tabs.addTab(self.tab3,"Notes")
+		self.tabs.addTab(self.tab4,"Skills")
  
 		# Create first tab
 		self.tab1.layout = QVBoxLayout(self)
@@ -43,6 +47,11 @@ class MultiTabWidget(QWidget):
 		self.tab3.layout = QVBoxLayout(self)
 		self.tab3.layout.addWidget(notes)
 		self.tab3.setLayout(self.tab3.layout)
+			
+		# Create third tab
+		self.tab4.layout = QVBoxLayout(self)
+		self.tab4.layout.addWidget(skills)
+		self.tab4.setLayout(self.tab4.layout)
 
 		# Add tabs to widget		
 		self.layout.addWidget(self.tabs)
