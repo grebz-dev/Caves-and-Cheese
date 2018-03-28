d57543013c72eeeb50f7d2ec6ee89cfd2d75c75a
Josh Grebler <jgreb314@gmail.com>
2018-03-28 11:56:30 -0400

    apparently NULL isn't a thing in python...

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player():
 		self.notes = {}
 		self.skills = {}
 		self.health = 20
-		self.traits = {"CHARACTER_NAME":NULL,"PLAYER_NAME":NULL,"STRENGTH":NULL,"CAPACITY":NULL,"HEALTH":NULL,"LEVEL":NULL}
+		self.traits = {"CHARACTER_NAME":'',"PLAYER_NAME":'',"STRENGTH":'',"CAPACITY":'',"HEALTH":'',"LEVEL":''}
 		self.parseStats(statFile)
 		self.initUI()
 		
@@ -43,7 +43,13 @@ class Player():
 			elif (key in self.traits):
 				self.traits[key] = value
 				del self.stats[key]
-				
+	
+	def updateStat(self, key, value):
+		self.stats[key]=value
+		
+	def updateInventory(self, item, index):
+		self.inventory[index]=item
+		print(self.inventory)
 	
 	def export(self, filename):
 		file = open(filename,'w+')
