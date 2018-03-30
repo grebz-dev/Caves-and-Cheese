8d8b9b480c078c9956805f736515cf034a293017
Josh Grebler <jgreb314@gmail.com>
2018-03-29 23:19:50 -0400

    Added custom signal

--- notes.py
+++ notes.py
@@ -1,18 +1,20 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPlainTextEdit
-
+from PyQt5.QtCore import pyqtSignal
 
 class NotesWidget(QWidget):
 
+	widgetUpdate=pyqtSignal()
+
 	def __init__(self,player):
-			super().__init__()
-			self.initUI(player)
+		super().__init__()
+		self.initUI(player)
 		
 	def initUI(self, player):
 		layout = QVBoxLayout()
-		textedit = QPlainTextEdit()
+		self.textedit = QPlainTextEdit()
 
-		textedit.textChanged.connect(self.valUpdate)
-		layout.addWidget(textedit)
+		self.textedit.textChanged.connect(self.valUpdate)
+		layout.addWidget(self.textedit)
 		self.setLayout(layout)
 		
 	def valUpdate(self):
