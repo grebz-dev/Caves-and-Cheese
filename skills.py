4bf81e603749505d56878911174f8a7bafc6309d
Josh Grebler <jgreb314@gmail.com>
2018-03-29 22:10:38 -0400

    Skill value updates now pushed back to player object

--- skills.py
+++ skills.py
@@ -7,22 +7,26 @@ class SkillWidget(QWidget):
 
 	widgetUpdate=pyqtSignal(str,int)
 
-	def __init__(self, skills):
-		self.skills = skills
+	def __init__(self, player):
 		super().__init__()
 		self.initUI()
 		
-	def initUI(self):
+	def initUI(self, player):
 		list = QListWidget()
 		vbox = QVBoxLayout()
-		for i in range(len(self.skills)):
-			item = ListLineWidget(self.skills[i],i)
+		
+		while len(player.skills) < 20:
+			player.skills.append("")
+		
+		for i in range(len(player.skills)):
+			item = ListLineWidget(player.skills[i],i)
 			item.widgetUpdate.connect(self.valUpdate)
 			listWidgetItem = QListWidgetItem(list)
 			list.addItem(listWidgetItem)
 			list.setItemWidget(listWidgetItem, item)
+			
 		vbox.addWidget(list)
 		self.setLayout(vbox)
 	
-	def valUpdate(self, name,index):
+	def valUpdate(self, name, index):
 		self.widgetUpdate.emit(name,index)
